e3dfd2eb04af4dc778121ef9dabd286b1b8ff012
Mohit-Kumar <my1362321@gmail.com>
2025-01-14 09:47:52 +0530

    //initial commit

---
+++ src/QUEUE/Q_reversal.java
@@ -0,0 +1,34 @@
+package QUEUE;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Q_reversal {
+    public static void Reverse(Queue<Integer> q){
+        Stack<Integer> s =new Stack<>();
+       while (!q.isEmpty()){
+           s.push(q.remove());
+
+       }
+       while (!s.isEmpty()){
+           q.add(s.pop());
+       }
+
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q =new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        Reverse(q);
+   while (!q.isEmpty()){
+       System.out.println(q.remove()+" ");
+   }
+        System.out.println();
+
+    }
+}
