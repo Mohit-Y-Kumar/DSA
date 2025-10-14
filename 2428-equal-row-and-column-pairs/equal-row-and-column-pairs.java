bed4b09583e1b811f37055692ad6b15499d79e34
Mohit Kumar <my1362321@gmail.com>
2025-10-14 17:05:39 +0530

    Time: 35 ms (46.28%) | Memory: 48.6 MB (83.07%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,21 +1,40 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        int count=0;
-        for(int r =0;r<grid.length;r++){
-            for(int c =0;c<grid.length;c++){
-                boolean isEqual =true;
-                for(int i =0;i<grid.length;i++){
-                    if(grid[r][i] != grid[i][c]){
-                        isEqual  =false;
-                    }
-                }
-                if(isEqual ==true){
-                    count++;
-                }
+        // int count=0;
+        // for(int r =0;r<grid.length;r++){
+        //     for(int c =0;c<grid.length;c++){
+        //         boolean isEqual =true;
+        //         for(int i =0;i<grid.length;i++){
+        //             if(grid[r][i] != grid[i][c]){
+        //                 isEqual  =false;
+        //             }
+        //         }
+        //         if(isEqual ==true){
+        //             count++;
+        //         }
+        //     }
+        // }
+
+        // return count;
+        
+        int n =grid.length;
+        int count =0;
+       HashMap<String,Integer> has =new HashMap<>();
+        for(int r =0;r<n;r++){
+         String row = Arrays.toString(grid[r]);
+            has.put(row, has.getOrDefault(row, 0) + 1);
+        }
+        for(int j =0;j<n;j++){
+            int[] col =new int[n];
+            for(int i =0;i<n;i++){
+                col[i] =grid[i][j];
+            }
+            String colVal = Arrays.toString(col);
+            if(has.containsKey(colVal)){
+              count +=has.get(colVal);
             }
         }
-
         return count;
-        
     }
+
 }
