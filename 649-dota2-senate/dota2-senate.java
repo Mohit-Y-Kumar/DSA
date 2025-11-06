3a53226a6f296f72813608de0894cdbb8d35a360
Mohit Kumar <my1362321@gmail.com>
2025-11-06 06:32:41 +0530

    Time: 10 ms (89.85%) | Memory: 47.1 MB (6.70%) - LeetSync

--- 649-dota2-senate/dota2-senate.java
+++ 649-dota2-senate/dota2-senate.java
@@ -1,47 +1,18 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-         StringBuilder sb = new StringBuilder(senate);
-        int rCount = 0, dCount = 0;
-
-        for (char c : sb.toString().toCharArray()) {
-            if (c == 'R') rCount++;
-            else dCount++;
+        Queue<Integer> qr = new LinkedList<>();
+        Queue<Integer> qd = new LinkedList<>();
+        int n = senate.length();
+        for(int i = 0;i < n;i++){
+            if(senate.charAt(i) == 'R') qr.add(i);
+            else qd.add(i);
         }
-
-        int banR = 0, banD = 0;
-
-        while (rCount > 0 && dCount > 0) {
-            StringBuilder nextRound = new StringBuilder();
-
-            for (int i = 0; i < sb.length(); i++) {
-                char c = sb.charAt(i);
-                if (c == '.') continue;
-
-                if (c == 'R') {
-                    if (banR > 0) {
-                        banR--;
-                        rCount--;
-                        continue; // R banned this round
-                    }
-                    // R acts → ban a D in next round
-                    banD++;
-                    nextRound.append('R');
-                } else { // D
-                    if (banD > 0) {
-                        banD--;
-                        dCount--;
-                        continue; // D banned this round
-                    }
-                    // D acts → ban an R in next round
-                    banR++;
-                    nextRound.append('D');
-                }
-            }
-
-            sb = nextRound; // prepare next round
+        while(!qr.isEmpty() && !qd.isEmpty()){
+            int r_i = qr.poll();
+            int d_i = qd.poll();
+            if(r_i < d_i) qr.add(r_i + n);
+            else qd.add(d_i + n);
         }
-
-        return rCount > 0 ? "Radiant" : "Dire";
+        return qr.size() > qd.size() ? "Radiant" : "Dire";
     }
 }
-    
