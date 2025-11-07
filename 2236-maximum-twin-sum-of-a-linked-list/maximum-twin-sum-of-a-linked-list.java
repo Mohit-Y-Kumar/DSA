1a2b7e6c115d050bd0f72adbec93c60344bb3433
Mohit Kumar <my1362321@gmail.com>
2025-11-07 07:58:27 +0530

    Time: 5 ms (61.10%) | Memory: 100.9 MB (5.74%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -10,19 +10,40 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        ArrayList<Integer> l =new ArrayList<>();
-        ListNode temp =head;
-        while(temp!=null){
-            l.add(temp.val);
-            temp =temp.next;
-           
+        ListNode slow =head,fast =head;
+        while(fast!=null && fast.next !=null ){
+            slow =slow.next;
+            fast =fast.next.next;
         }
-        int n =l.size();
-       int maxx =-1;
-        for(int i =0;i<n/2;i++){
-           int x =l.get(i)+ l.get(n-i-1);
-           maxx =Math.max(maxx,x);
+
+        //rev 2nd half
+        ListNode revsechalf =reverse(slow);
+
+        ListNode first = head;
+        ListNode second = revsechalf;
+        int maxx = -1;
+        while(second !=null){
+            int s =first.val +second.val;
+            maxx =Math.max(maxx,s);
+            first =first.next;
+            second =second.next;
+
         }
         return maxx;
     }
+
+        public ListNode reverse(ListNode head){
+            ListNode prev =null,curr =head,temp ;
+            while(curr!=null){
+                temp =curr.next;
+                curr.next =prev;
+                prev =curr;
+                curr =temp;
+            }
+            return prev;
+
+
+        }
+
+    
 }
