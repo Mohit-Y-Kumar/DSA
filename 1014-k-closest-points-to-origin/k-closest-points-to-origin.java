6ad01bb4d32817859f8d507bdb051c80a7dda260
Mohit Kumar <my1362321@gmail.com>
2025-11-27 01:14:16 +0530

    Time: 30 ms (62.02%) | Memory: 53.4 MB (60.69%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        if(points == null) return null;
+
+         PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+            (a, b) -> (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1])
+        );
+
+         for (int[] point : points) {
+            maxHeap.offer(point);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+
+         int[][] res = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            res[i] = maxHeap.poll();
+        }
+         return res;
+        
+    }
+}
