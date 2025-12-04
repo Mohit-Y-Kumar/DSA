ffa6178867a99ecd1e52a28198f5dac8bc82735f
Mohit Kumar <my1362321@gmail.com>
2025-12-04 05:57:01 +0530

    Time: 1 ms (98.23%) | Memory: 43.2 MB (47.85%) - LeetSync

--- 401-binary-watch/binary-watch.java
+++ 401-binary-watch/binary-watch.java
@@ -1,25 +1,54 @@
 class Solution {
-    
-    public List<String> readBinaryWatch(int num) {
-        if (num < 0 || num > 10) {
-            return new ArrayList<>();
+    List<String> res;
+    int[] clock;
+    public List<String> readBinaryWatch(int turnedOn) {
+         res = new ArrayList<>();
+         clock = new int[2];
+         backtrack(turnedOn, 0);
+         return res;
+    }
+
+    private void backtrack(int on, int p){
+        //the stop condition for backtracking
+        if(on == 0){
+            String time = getString(clock);
+            res.add(time);
+            return;
         }
-        
-        List<String> result = new ArrayList<>();
-      
-        for (int hour = 0; hour <= 11; hour++) {
-            for (int minute = 0; minute <= 59; minute++) {
-				
-				// If number of bits in hour + number of bits in minute equals num
-				// add to result or else do nothing.
-                if (Integer.bitCount(hour) + Integer.bitCount(minute) == num) {
-                    
-                    result.add(String.format("%d:%02d", hour, minute));
-                    
+        for(int i = p; i < 10; i++){
+            int num = 1 << (i % 6);
+            if(i < 6){
+                clock[1] += num;
+                if(isValid()){
+                    backtrack(on - 1, i + 1);
                 }
+                clock[1] -= num;
+            }else{
+                clock[0] += num;
+                if(isValid()){
+                    backtrack(on - 1, i + 1);
+                }
+                clock[0] -= num;
             }
-        }      
-        
-        return result;
+        }
+    }
+
+    //check if the clock has a valid time
+    private boolean isValid(){
+        if(clock[1] < 60 && clock[0] < 12){
+            return true;
+        }
+        return false;
+    }
+
+    private String getString(int[] mark){
+        StringBuilder sb = new StringBuilder();
+        sb.append(mark[0]);
+        sb.append(":");
+        if(mark[1] < 10){
+            sb.append(0);
+        }
+        sb.append(mark[1]);
+        return sb.toString();
     }
 }
