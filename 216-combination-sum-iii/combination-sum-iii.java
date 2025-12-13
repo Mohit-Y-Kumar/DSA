f8323c3b0bf5718171a2fcddfbc65cc57f5b0e07
Mohit Kumar <my1362321@gmail.com>
2025-12-13 06:06:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (15.36%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> subsets =new ArrayList<>();
+	genratecomb(1,9,new ArrayList(),subsets,k,n);
+	return subsets;
+    }
+	void genratecomb(int start,int end,List<Integer> current, List<List<Integer>> subsets,int k,int target){
+        if(target==0 && current.size()==k){
+        subsets.add(new ArrayList(current));
+        return;
+        }
+	
+        for(int i =start;i<=end;i++){
+        current.add(i);
+        genratecomb(i+1,end,current,subsets,k,target-i);
+        current.remove(current.size()-1);
+        }
+
+	}
+	
+}
