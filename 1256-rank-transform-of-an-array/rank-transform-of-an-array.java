5cd24ae69e03ad5d4d4505b7cfe717a799ecde10
Mohit Kumar <my1362321@gmail.com>
2026-03-26 19:54:55 +0530

    Time: 29 ms (84.78%) | Memory: 74.7 MB (85.28%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,19 +1,24 @@
 class Solution {
-    public int[] arrayRankTransform(int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int [] sortuniqnum=Arrays.stream(arr).distinct().sorted().toArray();
-        int rank =1;
-       for(int num:sortuniqnum){
-        map.put(num,rank++);
-       }
-       
-       
-       for(int i =0 ;i<arr.length;i++){
-        arr[i] =map.get(arr[i]);
-       }
-       
-
-      
-      return arr;
+    public int[] arrayRankTransform(int[] arr) 
+    {
+        int n = arr.length;
+        if (n == 0) return arr;
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+        int rank = 1;
+        HashMap<Integer, Integer> map = new HashMap<>(n * 2);
+        map.put(sorted[0], rank);
+        for (int i = 1; i < n; i++)
+        {
+            if (sorted[i] != sorted[i - 1])
+            {
+                map.put(sorted[i], ++rank);
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = map.get(arr[i]);
+        }
+        return arr;
     }
 }
