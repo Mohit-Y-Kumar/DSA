cd5fa14fc0aa9c4b8581618b6337430eb8204222
Mohit Kumar <my1362321@gmail.com>
2026-03-27 12:06:41 +0530

    Time: 1 ms (99.47%) | Memory: 47.3 MB (6.67%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -1,49 +1,22 @@
 class Solution {
     public boolean areSimilar(int[][] mat, int k) {
-        int m = mat.length;
-        int n = mat[0].length;
+        //Time Complexity - O(m*n)
+        //Space Complexity - O(1)
 
-        // store original matrix
-        int[][] original = new int[m][n];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                original[i][j] = mat[i][j];
-            }
-        }
-
-        // optimize k
-        k = k % n;
-
-        // apply shift k times
-        for(int i = 0; i < k; i++){
-            mat = shift(mat);
-        }
-
-        // compare with original
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(mat[i][j] != original[i][j]){
+        int m=mat.length;
+        int n=mat[0].length;
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int ind=(j+k)%n;
+                if(mat[i][j]!=mat[i][ind])
+                {
                     return false;
                 }
             }
         }
         return true;
-    }
-
-    public static int[][] shift(int[][] grid){
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] temp = new int[m][n];
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(i % 2 == 0){
-                    temp[i][j] = grid[i][(j + 1) % n]; // left shift
-                } else {
-                    temp[i][j] = grid[i][(j - 1 + n) % n]; // right shift
-                }
-            }
-        }
-        return temp;
     }
 }
