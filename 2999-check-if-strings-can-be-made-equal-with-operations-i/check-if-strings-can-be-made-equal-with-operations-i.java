34dae95fc775a2e6a6b3fe6c93ca741c63804f24
Mohit Kumar <my1362321@gmail.com>
2026-03-29 12:28:27 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (36.73%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,8 +1,26 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        return ((s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
-                (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0))) &&
-               ((s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
-                (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1)));
+      
+      int even[] =new int[26];
+      Arrays.fill(even,0);
+      int odd[] =new int[26];
+      Arrays.fill(odd,0);
+
+      for(int i =0;i<4;i++){
+        if(i%2==0){
+            even[s1.charAt(i) -'a']++;
+            even[s2.charAt(i) -'a']--;
+
+        }else{
+            odd[s1.charAt(i)-'a']++;
+            odd[s2.charAt(i)-'a']--;
+        }
+      }
+      for(int i =0;i<26;i++){
+        if(even[i] !=0 || odd[i] !=0){
+            return false;
+        }
+      }
+      return true;
     }
 }
