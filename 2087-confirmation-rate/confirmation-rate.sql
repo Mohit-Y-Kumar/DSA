b2fa766860e9495fe893a2e8fbc7194e7fc836f9
Mohit Kumar <my1362321@gmail.com>
2026-08-19 13:19:39 +0530

    Time: 702 ms (62.51%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -2,19 +2,13 @@
 select s.user_id,
  COALESCE(
         ROUND(
-            COALESCE(subq.confirmed_count, 0) / COALESCE(subq.total_count, 1),
+            SUM(c.action = 'confirmed') / COUNT(c.action),
             2
         ),
         0
     )AS  confirmation_rate
 from Signups s
-left join( 
-    select c.user_id,
-        SUM(c.action = 'confirmed') AS confirmed_count,
-        COUNT(*) AS total_count
-    from Confirmations c
-    GROUP BY c.user_id
-) subq
-ON s.user_id = subq.user_id;
-
+left join  Confirmations c
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
 
